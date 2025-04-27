228ee3a4d35c182c83eb8a8456903b372143f4c9
Nantou Paul <pro.paulon@gmail.com>
2025-04-27 10:02:55 +0200

    Navbar

--- client/components/AppNavigation.vue
+++ client/components/AppNavigation.vue
@@ -1,9 +1,33 @@
-<script>
+<script setup lang="ts">
 import '~/assets/css/AppNavigation.scss'
 </script>
-
 <template>
-    <div class="navbar bg-accented">
-        <UButton>Button</UButton>
+    <div class="navbar">
+        <div class="content  bg-indigo-800 bg-muted rounded-xl opacity-90">
+            <div class="left">
+                <img src="../public/logo_no_bg.png" alt="logo">
+            </div>
+            <div class="center">
+                <div class="buttons">
+                    <UButton
+                        icon="solar:home-2-bold"
+                        size="md"
+                        color="neutral"
+                        variant="outline"
+                    >
+                        Accueil
+                    </UButton>
+                    <UButton
+                        icon="solar:book-2-broken"
+                        size="md"
+                        color="neutral"
+                        variant="outline"
+                    >
+                        Docs
+                    </UButton>
+                </div>
+            </div>
+            <div class="right"></div>
+        </div>
     </div>
 </template>
