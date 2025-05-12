353ef4bb908fbde73483603245aea35b9d27c791
Nantou Paul <pro.paulon@gmail.com>
2025-05-12 16:40:59 +0200

    💄 Boutons navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -2,6 +2,15 @@
     <nav class="flex items-center p-4 bg-surface border-b border-border" style="position: relative;">
       <img style="width: 50px; margin-left: 20px;" src="/logo_sans_texte_sans_fond.png" alt="">
       <h1 class="text-xl font-bold ml-2">Serverly</h1>
-      <UButton style="position: absolute; right: 20px;" icon="solar:user-bold" label="Mon compte" color="primary" variant="solid" />
+
+      <div style="display: flex; position: absolute; right: 30px;" class="gap-5">
+        <UChip :show="false">
+         <UButton icon="ic:twotone-support" color="neutral" variant="subtle" />
+        </UChip>
+        <UChip :show="false">
+         <UButton icon="solar:bell-bold" color="neutral" variant="subtle" />
+        </UChip>
+        <UButton icon="solar:user-bold" label="Mon compte" color="primary" variant="solid" />
+      </div>
     </nav>
 </template>
