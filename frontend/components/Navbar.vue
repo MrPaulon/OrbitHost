ff32be6d708f1d2fba140f5fcdbb5e62625b3a5d
Nantou Paul <pro.paulon@gmail.com>
2025-05-12 16:24:52 +0200

    💄 Ajout logo navbar

--- frontend/components/Navbar.vue
+++ frontend/components/Navbar.vue
@@ -1,6 +1,7 @@
 <template>
-    <nav class="flex justify-between items-center p-4 bg-surface border-b border-border">
-      <h1 class="text-xl font-bold">Serverly</h1>
-      <UButton icon="solar:user-bold" label="Mon compte" color="primary" variant="soft" />
+    <nav class="flex items-center p-4 bg-surface border-b border-border" style="position: relative;">
+      <img style="width: 50px; margin-left: 20px;" src="/logo_sans_texte_sans_fond.png" alt="">
+      <h1 class="text-xl font-bold ml-2">Serverly</h1>
+      <UButton style="position: absolute; right: 20px;" icon="solar:user-bold" label="Mon compte" color="primary" variant="solid" />
     </nav>
 </template>
