094576e258f39df3391c3087f0af2827379e3acf
Nantou Paul <pro.paulon@gmail.com>
2025-05-13 08:52:08 +0200

    🚀 Middleware is_admin

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,8 +3,13 @@ const router = express.Router();
 
 const register = require('../controllers/users/register');
 const login = require('../controllers/users/login');
+const list = require('../controllers/users/list');
+
+const verifyToken = require('../middleware/auth');
+const isAdmin = require('../middleware/isAdmin');
 
 router.post('/register', register);
 router.post('/login', login);
+router.get('/list', verifyToken, isAdmin, list);
 
 module.exports = router;
