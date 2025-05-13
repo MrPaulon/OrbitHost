99f502a00115f67bd0578c8edb682f02a0ac0fde
Nantou Paul <pro.paulon@gmail.com>
2025-05-13 21:32:06 +0200

    🚀 Connexion api servers

--- frontend/pages/admin/servers/index.vue
+++ frontend/pages/admin/servers/index.vue
@@ -1,25 +1,43 @@
 <script setup lang="ts">
 
+const data = ref([
+  {
+    id: '',
+    user_id: '',
+    name: '',
+    ip_address: ''
+  },
+])
+
 definePageMeta({
   layout: 'admin'
 })
 
 import "~/assets/css/admin/servers/index.scss";
 
-const data = ref([
-  {
-    id: '4',
-    email: 'paul@serverly.fr',
-    pseudo: 'mrpaulon',
-    admin: 'true'
-  },
-])
 
 const globalFilter = ref('')
+
+onMounted(async () => {
+  const token = localStorage.getItem('token')
+  if (!token) return
+
+  try {
+    const response = await $fetch('http://localhost:3001/api/servers/list', {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
+    data.value = response
+  } catch (error) {
+    console.error('Erreur lors du chargement des serveurs :', error)
+  }
+})
 </script>
 <template>
   <div class="serverspage">
     <div class="content">
+      <h1>Liste des serveurs</h1>
       <div class="card">
         <div class="header">
           <UInput class="searchbar" v-model="globalFilter"  placeholder="Recherche" />
