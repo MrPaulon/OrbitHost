de0a186a039852e86fec66b8975353f36dcb484a
Nantou Paul <pro.paulon@gmail.com>
2025-05-21 21:27:07 +0200

    🚀 Mise en place panel gestion server

--- frontend/pages/server/[idServer]/index.vue
+++ frontend/pages/server/[idServer]/index.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>
-      <h1>Id: {{ idServer }}</h1>
+    <div class="serverpage dark">
+        <div class="content">
+            <h1>Id: {{ idServer }}</h1>
+        </div>
     </div>
 </template>
   
 <script setup lang="ts">
+// Style
+import "~/assets/css/server/index.scss"
+
+// Layout admin
+definePageMeta({
+  layout: 'server'
+})
+
 const route = useRoute()
 const idServer = route.params.idServer as string
 </script>
